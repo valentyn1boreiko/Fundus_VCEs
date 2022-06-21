3b6360889c7690b49eec765e434534d583cb357d
valentyn <valentyn.boreiko@gmail.com>
2022-06-21 14:38:30 +0200

    Update readme.md

--- utils_svces/train_types/helpers.py
+++ utils_svces/train_types/helpers.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn.functional as F
 from utils_svces.adversarial_attacks import PGD, MonotonePGD, APGDAttack, ArgminPGD, UniformNoiseGenerator,\
     NormalNoiseGenerator, L2FABAttack, LinfFABAttack, CutoutPGD
-#from blended_diffusion.optimization import DiffusionAttack
 from utils_svces.distances import LPDistance
 from torch.nn.modules.batchnorm import _BatchNorm
 from utils_svces.get_config import get_config
